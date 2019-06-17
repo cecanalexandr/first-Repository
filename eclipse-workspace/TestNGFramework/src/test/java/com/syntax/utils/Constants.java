2658246606aabaa913e2c4328b533e99b8c0b219
cecanalexandr <cecanalexandru0@gmail.com>
2019-06-17 14:31:05 -0400

    TestNGFramework

---
+++ eclipse-workspace/TestNGFramework/src/test/java/com/syntax/utils/Constants.java
@@ -0,0 +1,12 @@
+package com.syntax.utils;
+
+public class Constants {
+	public static final String CREDENTIALS_FILEPATH = "src\\test\\resources\\configs\\credentials.properties";
+
+	public static final String XL_FILEPATH="src/test/resources/TestDate/Book 1.xlsx";
+	
+	public static final String REPORT_FILEPATH="target/report/orangeHrm.html";
+	public static final String OS_NAME=System.getProperty("os.name");
+	public static final String USER_NAME=System.getProperty("user.name");
+
+}
